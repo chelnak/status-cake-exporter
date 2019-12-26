d383407705fbfdf9ae8e1ac6bdcf45a6e5d478e2
Craig Gumbley <craiggumbley@gmail.com>
2019-12-26 20:55:21 +0000

    Handle broken pipes rather than ignore

--- exporter/app.py
+++ exporter/app.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 
+import sys
+import os
 import time
 import logging
 
 from prometheus_client import start_http_server, REGISTRY
 from collectors import test_collector
 from utilities import logs, arguments
-from signal import signal, SIGPIPE, SIG_DFL
+
 
 if __name__ == "__main__":
 
-    signal(SIGPIPE, SIG_DFL)
-    args = arguments.get_args()
+    try:
 
-    logs.configure_logging(args.log_level)
-    logger = logging.getLogger(__name__)
+        args = arguments.get_args()
 
-    logger.info("Starting web server")
-    start_http_server(8000)
+        logs.configure_logging(args.log_level)
+        logger = logging.getLogger(__name__)
 
-    logger.info("Registering collectors")
-    REGISTRY.register(test_collector.TestCollector(
-        args.username, args.api_key, args.tags))
+        logger.info("Starting web server")
+        start_http_server(8000)
 
-    while True:
-        time.sleep(1)
+        logger.info("Registering collectors")
+        REGISTRY.register(test_collector.TestCollector(
+            args.username, args.api_key, args.tags))
+
+        while True:
+            time.sleep(1)
+
+        sys.stdout.flush()
+
+    except BrokenPipeError:
+
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, sys.stdout.fileno())
+        sys.exit(1)
