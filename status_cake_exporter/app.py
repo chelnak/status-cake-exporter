25d1afbbddb2a760bc861d2d651d1cecd53667dd
Craig Gumbley <craiggumbley@gmail.com>
2022-11-15 20:30:52 +0000

    Add paging parameter and fix logging

--- status_cake_exporter/app.py
+++ status_cake_exporter/app.py
@@ -9,6 +9,8 @@ from prometheus_client import REGISTRY, start_http_server
 from ._logging import configure_logging
 from ._test_collector import TestCollector
 
+logger = logging.getLogger("exporter")
+
 
 def exporter(
     username: str = typer.Option(
@@ -28,27 +30,36 @@ def exporter(
         envvar="LOG_LEVEL",
     ),
     port: int = typer.Option(8000, envvar="PORT"),
+    items_per_page=typer.Option(
+        25,
+        help="The number of items that the api will return on a page. This is a global option.",
+        envvar="ITEMS_PER_PAGE",
+    ),
 ):
 
     try:
         configure_logging(log_level)
-        logger = logging.getLogger(__name__)
 
         logger.info(f"Starting web server on port: {port}")
         start_http_server(port)
 
         logger.info("Registering collectors.")
-        test_collector = TestCollector(username, api_key, tags)
+        test_collector = TestCollector(username, api_key, items_per_page, tags)
         REGISTRY.register(test_collector)
 
         while True:
             time.sleep(1)
 
     except BrokenPipeError:
+        logger.fatal("Broken pipe error. Exiting.")
         devnull = os.open(os.devnull, os.O_WRONLY)
         os.dup2(devnull, sys.stdout.fileno())
         sys.exit(1)
+    except Exception as e:
+        logger.fatal(f"An unhandled exception occured: {e}")
+        exit(1)
     finally:
+        logger.debug("Shutting down.")
         sys.stdout.flush()
 
 
